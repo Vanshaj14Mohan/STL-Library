270ae77046c552affb36181330a55572f7179e33
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-20 23:57:01 +0530

    STL Intro

--- Extra Part/Containers/vec_iter.cpp
+++ Extra Part/Containers/vec_iter.cpp
@@ -8,7 +8,7 @@ int main(){
     vector<int> vec = {1, 2, 3, 4, 5};
     cout << "Vector begin with: " << *(vec.begin()) << endl; // Vector begin with: 1
     cout << "Vector ends with: " << *(vec.end()) << endl; // Might return 0 or any random value
-    // Defining an iterator
+    // Defining an iterator in C++
     vector<int> :: iterator it;
     for(it = vec.begin(); it != vec.end(); it ++){
         cout << *(it) << " "; // *(it) as it is a iterator so de refencing so that to know where it is pointing and the value it holds
