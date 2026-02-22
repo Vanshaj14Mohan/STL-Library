f02064e8309b9b21229819498967b50127e532f0
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-22 20:18:35 +0530

    STL Intro

--- Extra Part/Containers/list.cpp
+++ Extra Part/Containers/list.cpp
@@ -8,10 +8,18 @@ int main(){
     list<int> l;
     l.push_back(1);
     l.push_back(2);
+    l.emplace_back(3);
     l.push_front(4);
     l.push_front(5);
+    l.emplace_front(6);
     for(int val: l){
-        cout << val << " "; // 5 4 1 2
+        cout << val << " "; // 6 5 4 1 2 3 
     }
     cout << endl;
+    // Removing elements
+    l.pop_front(); // 6 would be removed
+    l.pop_back(); // 3 would be removed
+     for(int val: l){
+        cout << val << " "; // 5 4 1 2 
+    }
 }
