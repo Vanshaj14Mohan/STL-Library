5db6dd0376762e2550f98489fbe6a924fac790b5
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-25 22:25:00 +0530

    STL Intro

--- Extra Part/Non Sequential Containers/stack.cpp
+++ Extra Part/Non Sequential Containers/stack.cpp
@@ -5,11 +5,20 @@ using namespace std;
 
 int main(){
     // Creating a stack
-    stack<int> s;
+    stack<int> s; // an empty stack 
     s.push(1);
     s.push(2);
     s.push(3);
     cout << "Stack top => " << s.top() << endl;
     cout << "Stack size=> " << s.size() << endl;
+    // checking if stack is empty or not
+    cout <<"Check if stack is empty or not: " << s.empty() << endl; // return 0 as it's not empty
+    // Iterating over stack
+    cout<< "Iterating over stack "<< endl;
+    while(!s.empty()){
+        cout << s.top()<< " ";
+        s.pop();
+    }
+    cout << endl;
     return 0;
 }
