611fe3a18644809ff029084716aa99375dc22fe6
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2026-02-26 22:26:25 +0530

    STL Intro

--- Extra Part/Non Sequential Containers/map.cpp
+++ Extra Part/Non Sequential Containers/map.cpp
@@ -10,6 +10,7 @@ int main(){
     m["bed"] = 50;
     m["phone"] = 150;
     m["watch"] = 90;
+    //m.erase("tv"); // tv would be erased fromm the map 
     // Inserting elements in map, using insert and emplace
     m.insert({"camera", 90});
     m.emplace("shoes", 90);
@@ -20,4 +21,15 @@ int main(){
     cout << "Map Size: " << m.size() << endl; // 7
     // checking count
     cout << "Count for laptop key: " << m.count("laptop") <<endl; // 1; returns the number of keys that exist in our map
+    // and to print it's value
+    cout << "Count of laptop values: " << m["laptop"] << endl; // 50;
+    // checking if map is empty or not
+    cout << "checking if map is empty or not: " << m.empty() << endl; // return 0 as non empty
+    if(m.find("watch") != m.end()){
+        cout << "Watch found:\n";
+    }
+    else{
+        cout << "Not found\n";
+    }
+    return 0;
 }
